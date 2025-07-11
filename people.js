d63dc715ada7924ea16ffe3df59c7844362c1db2
GeorgeMac <60342692+HeskeyML@users.noreply.github.com>
2025-07-11 18:41:46 +0100

    Create people.js

---
+++ people.js
@@ -0,0 +1,7 @@
+const people = [
+    { name: "Albert Einstein", birth: "1879-03-14", death: "1955-04-18" },
+    { name: "Marie Curie", birth: "1867-11-07", death: "1934-07-04" },
+    { name: "Isaac Newton", birth: "1643-01-04", death: "1727-03-31" },
+    { name: "Ada Lovelace", birth: "1815-12-10", death: "1852-11-27" },
+    { name: "Nikola Tesla", birth: "1856-07-10", death: "1943-01-07" }
+];
